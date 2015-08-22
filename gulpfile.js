5977a840924428d43f74d6aaf7aa3baeadfd3870
Andrew Hathaway <andrew@andrewhathaway.net>
2015-08-22 08:15:45 +0100

    Remove DIST + dist build tasks, as well as remove babelify and add dev dependancies

--- gulpfile.js
+++ gulpfile.js
@@ -2,34 +2,8 @@ var gulp       = require('gulp');
 var browserify = require('browserify');
 var source     = require('vinyl-source-stream');
 var reactify   = require('reactify');
-var babelify   = require('babelify');
 var buffer     = require('vinyl-buffer');
 
-/**
- * Dist Build
- */
-gulp.task('build-dist', function() {
-  return browserify({
-           debug   : process.env.NODE_ENV != 'production',
-           entries : [
-             'src/index.js'
-           ]
-         })
-         .transform(babelify)
-         .bundle()
-         .pipe(source('winterfell.js'))
-         .pipe(buffer())
-         .pipe(gulp.dest('dist'));
-});
-
-gulp.task('watch-dist', function() {
-  return gulp.watch([
-    'src/**/*.js',
-    '!dist/winterfell.js'
-  ], {}, function() {
-    return gulp.start('build-dist');
-  });
-});
 
 /**
  * Dev Build
