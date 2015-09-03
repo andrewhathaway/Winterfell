cea17c134c647e548ca3ac5e00bba2633bfc877e
Andrew Hathaway <andrew@andrewhathaway.net>
2015-09-03 19:19:36 +0100

    Remove unused dev folder

--- gulpfile.js
+++ gulpfile.js
@@ -32,30 +32,3 @@ gulp.task('watch-examples', function() {
     return gulp.start('build-examples');
   });
 });
-
-/**
- * Dev Build
- */
-gulp.task('build-dev', function() {
-  return browserify({
-           debug   : process.env.NODE_ENV != 'production',
-           entries : [
-             'dev/test.js'
-           ]
-         })
-         .transform(babelify)
-         .bundle()
-         .pipe(source('app.js'))
-         .pipe(buffer())
-         .pipe(gulp.dest('dev/build'));
-});
-
-gulp.task('watch-dev', function() {
-  return gulp.watch([
-    'src/**/*.js',
-    'dev/**/*.js',
-    '!dev/build/app.js'
-  ], {}, function() {
-    return gulp.start('build-dev');
-  });
-});
