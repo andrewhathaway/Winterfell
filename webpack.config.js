b63c33a8545832802b88911c755a04ca09f3fe85
Andrew Hathaway <andrew@andrewhathaway.net>
2015-09-19 17:34:18 +0100

    Cleanup formatting etc of webpack.config.js to match Winterfells code style

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,35 @@
 const webpack = require('webpack');
-const path = require('path');
+const path    = require('path');
 
 module.exports = {
-  context: __dirname + '/src',
-  entry: {
-    javascript: "./index.js"
+  context   : __dirname + '/src',
+  entry     : {
+    javascript : './index.js'
   },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loaders: ["babel-loader"],
-      }
-    ],
+  module    : {
+    loaders : [{
+      test    : /\.js$/,
+      exclude : /node_modules/,
+      loaders : ['babel-loader'],
+    }],
   },
-  externals: {
-    'react': 'React',
-    'react/addons': 'React'
+  externals : {
+    'react'        : 'React',
+    'react/addons' : 'React'
   },
-  output: {
-    libraryTarget: "var",
-    library: "Winterfell",
-    filename: "winterfell.min.js",
-    path: __dirname + "/dist"
+  output    : {
+    libraryTarget : 'var/',
+    library       : 'Winterfell',
+    filename      : 'winterfell.min.js',
+    path          : __dirname + '/dist'
   },
-  plugins: [
+  plugins  : [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}})
+    new webpack.optimize.UglifyJsPlugin({
+      compress : {
+        warnings : false
+      }
+    })
   ]
-}
+};
