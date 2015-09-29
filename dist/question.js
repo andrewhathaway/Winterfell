e3bcaafed3d0b9a8f06cea05ec7cab9809339456
algm <algm85@gmail.com>
2015-09-29 16:30:12 +0200

    Adding option for passing custom props to input components.

--- dist/question.js
+++ dist/question.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
 var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
@@ -85,6 +87,12 @@ var Question = (function (_React$Component) {
         );
       }) : [];
 
+      var extraprops = {};
+
+      if (this.props.input.props) {
+        extraprops = this.props.input.props;
+      }
+
       return React.createElement(
         'div',
         { className: this.props.classes.question },
@@ -99,7 +107,7 @@ var Question = (function (_React$Component) {
           this.props.text
         ) : undefined,
         validationErrors,
-        React.createElement(Input, { name: this.props.questionId,
+        React.createElement(Input, _extends({ name: this.props.questionId,
           value: value,
           text: this.props.input.text,
           options: this.props.input.options,
@@ -108,7 +116,8 @@ var Question = (function (_React$Component) {
           classes: this.props.classes,
           onChange: this.handleInputChange.bind(this, this.props.questionId),
           onBlur: this.handleInputBlur.bind(this, this.props.questionId),
-          onKeyDown: this.props.onKeyDown }),
+          onKeyDown: this.props.onKeyDown
+        }, extraprops)),
         !!this.props.postText ? React.createElement(
           'p',
           { className: this.props.classes.questionPostText },
